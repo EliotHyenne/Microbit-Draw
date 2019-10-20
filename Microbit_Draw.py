d56974f045ec9108caa66e5bc1b5673cdd103b90
Eliot <34645836+EliotHyenne@users.noreply.github.com>
2019-10-20 23:22:32 +0200

    Add files via upload

--- Microbit_Draw.py
+++ Microbit_Draw.py
@@ -7,27 +7,27 @@ editing = True
 
 def newPoint(x, y):
     points.append([x, y])
-    
-def removePoint(x, y, array):
-    for i in range(len(array)):
-        if array[i][0] == x and array[i][1] == y:
-            array.remove(array[i])
+
+def removePoint(x, y):
+    for i in range(len(points)):
+        if points[i][0] == x and points[i][1] == y:
+            points.remove(points[i])
             break
-    
+
 def setPoints(array):
     if array:
         for i in range(len(array)):
             microbit.display.set_pixel(array[i][0], array[i][1], 9)
 
-def freeze(array):
+def freeze():
     editing = False
     microbit.display.clear()
-    
+
     while not editing:
-        setPoints(array)
+        setPoints(points)
         if (microbit.button_a.was_pressed()):
             editing = True
-    
+
 def updatePos(x, y):
     x = 2
     y = 2
@@ -53,26 +53,25 @@ def updatePos(x, y):
         y = 3
     elif (microbit.accelerometer.get_y() > 550):
         y = 4
-        
+
     microbit.display.clear()
     microbit.display.set_pixel(x, y, 9)
-        
+
     if microbit.button_b.was_pressed():
         newPoint(x, y)
     elif microbit.button_a.was_pressed():
         if points:
             for i in range(len(points)):
                 if points[i][0] == x and points[i][1] == y:
-                    removePoint(x, y, points)
+                    removePoint(x, y)
                     break
 
 while editing:
     updatePos(posX, posY)
     setPoints(points)
-    
+
     if (microbit.accelerometer.is_gesture("shake")):
          points.clear()
-    
+
     if (microbit.button_a.is_pressed() and microbit.button_b.is_pressed()):
-        freeze(points)
-    
+        freeze()
