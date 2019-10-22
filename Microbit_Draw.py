c1b116a8643d4c154d76c444c9f8040a3c37d3d0
Eliot <34645836+EliotHyenne@users.noreply.github.com>
2019-10-22 23:52:11 +0200

    Add files via upload

--- Microbit_Draw.py
+++ Microbit_Draw.py
@@ -1,9 +1,35 @@
 import microbit
 
-posX = 2
-posY = 2
 points = []
+grid = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
 editing = True
+
+def displayGrid():
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            microbit.display.set_pixel(j, i, grid[i][j])
+
+def clearGrid():
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            grid[i][j] = 0;
+
+def displayCursor(x, y):
+    grid[y][x] = 5
+    displayGrid()
+    grid[y][x] = 0
+
+def displayPoint(x, y):
+    grid[y][x] = 9
+
+def displayPoints(e):
+    if e and points:
+        for i in range(len(points)):
+            displayPoint(points[i][0], points[i][1])
+    elif not e and points:
+        for i in range(len(points)):
+            displayPoint(points[i][0], points[i][1])
+            displayGrid()
 
 def newPoint(x, y):
     points.append([x, y])
@@ -11,48 +37,42 @@ def newPoint(x, y):
 def removePoint(x, y):
     points.remove([x, y])
 
-def setPoints(array):
-    if array:
-        for i in range(len(array)):
-            microbit.display.set_pixel(array[i][0], array[i][1], 9)
-
 def freeze():
     editing = False
-    microbit.display.clear()
+    clearGrid()
 
     while not editing:
-        setPoints(points)
+        displayPoints(editing)
         if (microbit.button_a.was_pressed()):
             editing = True
 
-def updatePos(x, y):
+def updatePos():
     x = 2
     y = 2
 
-    if microbit.accelerometer.get_x() <= -550:
+    if (microbit.accelerometer.get_x() <= -550):
         x = 0
-    elif -550 < microbit.accelerometer.get_x() <= -250:
+    elif (-550 < microbit.accelerometer.get_x() <= -250):
         x = 1
-    elif -250 < microbit.accelerometer.get_x() <= 250:
+    elif (-250 < microbit.accelerometer.get_x() <= 250):
         x = 2
-    elif 250 < microbit.accelerometer.get_x() <= 550:
+    elif (250 < microbit.accelerometer.get_x() <= 550):
         x = 3
-    elif microbit.accelerometer.get_x() > 550:
+    elif (microbit.accelerometer.get_x() > 550):
         x = 4
 
-    if microbit.accelerometer.get_y() <= -550:
+    if (microbit.accelerometer.get_y() <= -550):
         y = 0
-    elif -550 < microbit.accelerometer.get_y() <= -250:
+    elif (-550 < microbit.accelerometer.get_y() <= -250):
         y = 1
-    elif -250 < microbit.accelerometer.get_y() <= 250:
+    elif (-250 < microbit.accelerometer.get_y() <= 250):
         y = 2
-    elif 250 < microbit.accelerometer.get_y() <= 550:
+    elif (250 < microbit.accelerometer.get_y() <= 550):
         y = 3
-    elif microbit.accelerometer.get_y() > 550:
+    elif (microbit.accelerometer.get_y() > 550):
         y = 4
 
-    microbit.display.clear()
-    microbit.display.set_pixel(x, y, 9)
+    displayCursor(x, y)
 
     if microbit.button_b.was_pressed():
         newPoint(x, y)
@@ -64,11 +84,12 @@ def updatePos(x, y):
                     break
 
 while editing:
-    updatePos(posX, posY)
-    setPoints(points)
+    updatePos()
+    displayPoints(editing)
 
     if (microbit.accelerometer.is_gesture("shake")):
-         points.clear()
-
-    if (microbit.button_a.is_pressed() and microbit.button_b.is_pressed()):
+        points.clear()
+        clearGrid()
+        displayGrid()
+    elif (microbit.button_a.is_pressed() and microbit.button_b.is_pressed()):
         freeze()
